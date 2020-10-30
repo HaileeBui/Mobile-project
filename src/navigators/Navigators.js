f9ba1d2885fc68c417e89c66475121b6d5da9cad
Misty Bui <mishtybui@gmail.com>
2020-10-30 10:26:43 +0200

    sos ui

---
+++ src/navigators/Navigators.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import Map from '../views/Map';
+import Sos from '../views/Sos';
+import Home from '../views/Home';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+const Navigators = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            title: '',
+            headerStyle: {
+              backgroundColor: '#ffc13b',
+            },
+            headerTintColor: '#1e3d59',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+        />
+        <Stack.Screen
+          name="Map"
+          component={Map}
+          options={{
+            title: '',
+            headerStyle: {
+              backgroundColor: '#ffc13b',
+            },
+            headerTintColor: '#1e3d59',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+        />
+        <Stack.Screen
+          name="Sos"
+          component={Sos}
+          options={{
+            title: '',
+            headerStyle: {
+              backgroundColor: '#ffc13b',
+            },
+            headerTintColor: '#1e3d59',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+export default Navigators
---
+++ src/views/Map.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {Text} from 'native-base'
+const Map = () => {
+  return (
+    <Text>this is map page</Text>
+  )
+}
+
+export default Map
