9043eb8573898029a091f1992ece4a48929760ed
Anwar Ulhaq <mailtoanwar@gmail.com>
2020-11-11 09:38:32 +0200

    Simple Google map added.

--- src/views/Map.js
+++ src/views/Map.js
@@ -1,9 +1,47 @@
-import React from 'react'
-import {Text} from 'native-base'
+import React, { useEffect} from 'react';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import {
+  StyleSheet,
+  View,
+  Dimensions,
+} from 'react-native';
+
 const Map = () => {
+
+  const { width, height } = Dimensions.get('window');
+  const ASPECT_RATIO = width / height;
+
+  useEffect(() => {
+  });
+
   return (
-    <Text>this is map page</Text>
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        initialRegion={
+          {
+            latitude: 60.161822,
+            longitude: 24.917335,
+            latitudeDelta: 0.001,
+            longitudeDelta: 0.001 * ASPECT_RATIO,
+          }
+        }
+      >
+      </MapView>
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
 
 export default Map
