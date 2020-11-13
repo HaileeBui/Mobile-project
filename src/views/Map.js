dc717b235ffe3e780140a38bef931846fa2396b1
Anwar Ulhaq <mailtoanwar@gmail.com>
2020-11-13 15:41:09 +0200

    User location added to map.

--- src/views/Map.js
+++ src/views/Map.js
@@ -1,4 +1,4 @@
-import React, { useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import {
   StyleSheet,
@@ -10,27 +10,58 @@ const Map = () => {
 
   const { width, height } = Dimensions.get('window');
   const ASPECT_RATIO = width / height;
+  const LATITUDE_DELTA = 0.005;
+  const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+  const [ lastLatitude, setLastLatitude ] = useState(null);
+  const [ lastLongitude, setLastLongitude ] = useState(null);
+  const [ lastHeading, setLastHeading ] = useState(null);
+  const [ lastSpeed, setLastSpeed ] = useState(null);
 
   useEffect(() => {
+
+    let watchID = navigator.geolocation.watchPosition(
+      //successCallback
+      ({coords}) => {
+        setLastLatitude ( coords.latitude );
+        setLastLongitude ( coords.longitude );
+        setLastHeading ( coords.heading );
+        setLastSpeed ( coords.speed );
+      },
+      //errorCallBack
+      (error) => {
+        console.log("Error: " + error.message);
+      },
+      //options:
+      {
+        enableHighAccuracy: true,
+        maximumAge: 1000,
+        distanceFilter: 50,
+      },
+    );
+
+    return () => {
+      navigator.geolocation.clearWatch(watchID);
+    };
   });
 
   return (
-    <View style={styles.container}>
-      <MapView
-        provider={PROVIDER_GOOGLE}
-        style={styles.map}
-        initialRegion={
-          {
-            latitude: 60.161822,
-            longitude: 24.917335,
-            latitudeDelta: 0.001,
-            longitudeDelta: 0.001 * ASPECT_RATIO,
-          }
-        }
-      >
-      </MapView>
-    </View>
-  )
+  <View style={styles.container}>
+    <MapView
+      provider={PROVIDER_GOOGLE}
+      style={styles.map}
+      region={{
+        latitude: ( lastLatitude ) || 60.161822,
+        longitude: (lastLongitude ) || 24.917335,
+        latitudeDelta:  LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA
+      }}
+      showsUserLocation={true}
+      followUserLocation={true}
+    >
+    </MapView>
+  </View>
+)
 }
 
 const styles = StyleSheet.create({
