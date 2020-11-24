5391a8f2795d6fc93f4f32f6a7e3ccf7bbaba03a
Anwar Ulhaq <mailtoanwar@gmail.com>
2020-11-24 13:04:31 +0200

    Refactor code for displaying weather data.

--- src/views/Map.js
+++ src/views/Map.js
@@ -1,36 +1,29 @@
 import React, {useEffect, useState} from 'react';
-import MapView, { Callout, Circle, PROVIDER_GOOGLE, } from 'react-native-maps';
-import {
-  StyleSheet,
-  View,
-  Dimensions,
-  Text,
-} from 'react-native';
+import MapView, {Callout, Circle, PROVIDER_GOOGLE} from 'react-native-maps';
+import Constants from 'expo-constants';
+import {Text, Container, View} from 'native-base';
 import firebase from 'firebase';
-import { DOMParser } from 'xmldom'
+import { StyleSheet, Dimensions, Switch } from 'react-native';
 
-import Boat from '../components/Boat';
-import { digiTrafficService, firebaseService } from '../services';
-import { Distance } from '../utilities';
+import { Boat, WeatherContainer } from '../components';
+import {digiTrafficService, firebaseService} from '../services';
+import {Distance} from '../utilities';
+import {mapStyles} from '../styles';
 
+
+const apiURL = 'https://pfa.foreca.com';
 
 const Map = () => {
 
-  const { width, height } = Dimensions.get('window');
+  const {width, height} = Dimensions.get('window');
   const ASPECT_RATIO = width / height;
   const LATITUDE_DELTA = 0.005;
   const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
   const METER_TO_KILOMETER_CONSTANT = 3.6;
   const METER_TO_KNOT_CONSTANT = 1.9438445;
 
-  const [ lastLatitude, setLastLatitude ] = useState(60.161822);
-  const [ lastLongitude, setLastLongitude ] = useState(24.917335);
-  const [ lastHeading, setLastHeading ] = useState(0);
-  const [ lastSpeed, setLastSpeed ] = useState(0);
-  const [ vessels, setVessels ] = useState([]);
-  const [ isCollisionDetected, setIsCollisionDetected ] = useState(false);
-  const [ alertRadius , setAlertRadius ] = useState(0.900); // In Kilometers
-  const [ isDarkModeEnabled, setIsDarkModeEnabled ] = useState(false);
+  const [location, setLocation] = useState(null);
+  const [weather, setWeather] = useState({current: {}});
   const [lastLatitude, setLastLatitude] = useState(60.161822);
   const [lastLongitude, setLastLongitude] = useState(24.917335);
   const [lastHeading, setLastHeading] = useState(0);
@@ -38,18 +31,22 @@ const Map = () => {
   const [vessels, setVessels] = useState([]);
   const [isCollisionDetected, setIsCollisionDetected] = useState(false);
   const [alertRadius, setAlertRadius] = useState(0.900); // In Kilometers
+  const [isDarkModeEnabled, setIsDarkModeEnabled] = useState(false);
 
   const loadVessels = () => {
 
-    firebaseService.getAllVessels()
-      .then(vessels => {
-        setVessels(vessels);
-        const proximityAlert = vessels.some(vessel => Distance.isDistanceLessThen(vessel, lastLatitude, lastLongitude, alertRadius));
-        setIsCollisionDetected(proximityAlert);
-      });
-  }
+    firebaseService.getAllVessels().then(vessels => {
+      setVessels(vessels);
+      const proximityAlert = vessels.some(
+        vessel => Distance.isDistanceLessThen(vessel, lastLatitude,
+          lastLongitude, alertRadius));
+      setIsCollisionDetected(proximityAlert);
+    });
+  };
 
-  const toggleSwitch = () => setIsDarkModeEnabled(previousState => !previousState);
+  const toggleSwitch = () => setIsDarkModeEnabled(
+    previousState => !previousState);
+
   /*const getToken = () => {
     const data = { user: 'ngoc-bui', password: 'yySqoYelUSsKuPHoaP' }
     fetch(apiURL + '/authorize/token?user=' + data.user + '&password=' + data.password)
@@ -91,41 +88,42 @@ const Map = () => {
       navigator.geolocation.getCurrentPosition(
         position => {
           try {
-            fetch(apiURL + '/api/v1/current/' + position.coords.longitude + ',' + position.coords.latitude + '?token=' + newToken.access_token)
-              .then(response => response.json()
-                .then(
-                  result =>
-                    setWeather(result),
-                  error => console.log(error)
-                ))
+            fetch(
+              apiURL + '/api/v1/current/' + position.coords.longitude + ',' +
+              position.coords.latitude + '?token=' + newToken.access_token).
+              then(response => response.json().then(
+                result =>
+                  setWeather(result),
+                error => console.log(error),
+              ));
           } catch (e) {
             console.log('fetch weather error', e);
           }
-        })
+        });
     } catch (error) {
       console.error('fetching token error', error);
     }
-  }
+  };
 
   useEffect(() => {
     getWeather();
     loadVessels();
 
     let watchID = navigator.geolocation.watchPosition(
-
       //successCallback
-      ({ coords }) => {
+      ({coords}) => {
 
         setLastLatitude(coords.latitude);
         setLastLongitude(coords.longitude);
         setLastHeading(coords.heading);
         setLastSpeed(coords.speed);
 
-        firebaseService.updateVessel(coords.latitude, coords.longitude, coords.heading, coords.speed)
+        firebaseService.updateVessel(coords.latitude, coords.longitude,
+          coords.heading, coords.speed);
       },
       //errorCallBack
       (error) => {
-        console.log("Error: " + error.message);
+        console.log('Error: ' + error.message);
       },
       //options:
       {
@@ -136,39 +134,37 @@ const Map = () => {
     );
 
     //child_changed
-    firebase.database()
-      .ref('/vessels')
-      .on('child_changed', snapshot => {
+    firebase.database().ref('/vessels').on('child_changed', snapshot => {
 
-        if (!(snapshot.val().userId === firebase.auth().currentUser.uid)) {
-          const updateVessel = {
-            id: snapshot.key,
-            latitude: snapshot.val().latitude,
-            longitude: snapshot.val().longitude,
-            heading: snapshot.val().heading,
-            speed: snapshot.val().speed,
-            hasMayDay: snapshot.val().hasMayDay,
-          };
+      if (!(snapshot.val().userId === firebase.auth().currentUser.uid)) {
+        const updateVessel = {
+          id: snapshot.key,
+          latitude: snapshot.val().latitude,
+          longitude: snapshot.val().longitude,
+          heading: snapshot.val().heading,
+          speed: snapshot.val().speed,
+          hasMayDay: snapshot.val().hasMayDay,
+        };
 
-          const updateVessels = vessels.map(vessel => {
+        const updateVessels = vessels.map(vessel => {
 
-            if (vessel.id === snapshot.val().userId) {
-              return updateVessel;
-            }
-
-            return vessel;
-          });
-
-          const proximityAlert = Distance.isDistanceLessThen(updateVessel,
-            lastLatitude, lastLongitude, alertRadius);
-
-          if (proximityAlert) {
-            setIsCollisionDetected(proximityAlert);
+          if (vessel.id === snapshot.val().userId) {
+            return updateVessel;
           }
 
-          setVessels(updateVessels);
+          return vessel;
+        });
+
+        const proximityAlert = Distance.isDistanceLessThen(updateVessel,
+          lastLatitude, lastLongitude, alertRadius);
+
+        if (proximityAlert) {
+          setIsCollisionDetected(proximityAlert);
         }
-      });
+
+        setVessels(updateVessels);
+      }
+    });
 
     //TODO child_add, child_removed
 
@@ -177,56 +173,12 @@ const Map = () => {
       firebaseService.detachAllFirebaseCallbacks();
       navigator.geolocation.clearWatch(watchID);
     };
-  }, [])
+  }, []);
+
   return (
     <Container>
-      {Object.keys(weather.current).length > 0 &&
-        <Card>
-          <CardItem style={{ backgroundColor: '#f5f0e1' }}>
-            <Left>
-              <Icon name='md-thermometer' />
-              <Body>
-                <Left></Left>
-                <Text style={{ fontWeight: 'bold', fontSize: 20 }}>Temperature: {weather.current.temperature}°C</Text>
-                <Text>Feels like: {weather.current.feelsLikeTemp}°C</Text>
-              </Body>
-            </Left>
-            <Body>
-              <Left>
-                <Icon name='md-information-circle-outline' />
-              </Left>
-              <Right>
-                <Text style={{ fontSize: 18 }}>{weather.current.symbolPhrase}</Text>
-              </Right>
-            </Body>
-          </CardItem>
-          <CardItem style={{ backgroundColor: '#f5f0e1' }}>
-            <Left>
-              <Left />
-              <Body>
-                <Icon name='md-water' />
-                <Text>{weather.current.relHumidity}%</Text>
-              </Body>
-            </Left>
-            <Body>
-              <Body>
-                <Icon name='md-speedometer' />
-                <Text>{weather.current.windSpeed} m/s, {weather.current.windDirString}</Text>
-
-              </Body>
-            </Body>
-            <Right>
-              <Body>
-                <Icon name='md-eye' style={{ color: 'black' }} />
-                <Text>{weather.current.visibility}</Text>
-
-              </Body>
-              <Right />
-            </Right>
-          </CardItem>
-        </Card>
-      }
-      <View style={styles.container}>
+      <WeatherContainer weather={weather}/>
+      <View style={styles.mapContainer}>
         <MapView
           provider={PROVIDER_GOOGLE}
           style={styles.map}
@@ -234,9 +186,10 @@ const Map = () => {
             latitude: lastLatitude,
             longitude: lastLongitude,
             latitudeDelta: LATITUDE_DELTA,
-            longitudeDelta: LONGITUDE_DELTA
+            longitudeDelta: LONGITUDE_DELTA,
           }}
           showsUserLocation={true}
+          customMapStyle={(isDarkModeEnabled ? mapStyles.darkMode : [])}
         >
           <Boat
             key={0}
@@ -248,77 +201,7 @@ const Map = () => {
             isInDanger={isCollisionDetected}
             isThisUser={true}
           />
-  <View style={styles.container}>
-    <MapView
-      provider={PROVIDER_GOOGLE}
-      style={styles.map}
-      region={{
-        latitude:  lastLatitude ,
-        longitude: lastLongitude ,
-        latitudeDelta:  LATITUDE_DELTA,
-        longitudeDelta: LONGITUDE_DELTA
-      }}
-      showsUserLocation={true}
-      customMapStyle={( isDarkModeEnabled ? mapStyles.darkMode : [] )}
-    >
-      <Boat
-        key={0}
-        latitude={ lastLatitude }
-        longitude={ lastLongitude }
-        heading={ lastHeading }
-        //speed={ lastSpeed }
-        hasMayDay={false}
-        isInDanger={ isCollisionDetected }
-        isThisUser={true}
-      />
 
-      {vessels.map( (vessel, index) => (
-        <Boat
-          key={vessel.id}
-          latitude={vessel.latitude}
-          heading={vessel.heading}
-          longitude={vessel.longitude}
-          speed={vessel.speed}
-          hasMayDay={vessel.hasMayDay}
-          title={vessel.id}
-        />
-      ))}
-
-      {/*{ isCollisionDetected && <Circle
-        center={{
-          latitude: lastLatitude ,
-          longitude: lastLongitude ,
-        }}
-        //enter={alertZone.center}
-        radius={alertRadius * 1000}
-        fillColor={'rgba(255, 0, 0, 0.2)'}
-        strokeColor="rgba(0,0,0,0.5)"
-      /> }*/}
-    </MapView>
-
-    <View style={styles.speedContainer}>
-      <Text style={styles.bubble}>
-        {lastSpeed * METER_TO_KILOMETER_CONSTANT } Km/h,
-      </Text>
-      <Text style={styles.bubble}>
-        {lastSpeed * METER_TO_KNOT_CONSTANT} Knots
-      </Text>
-    </View>
-    <View style={styles.switchContainer}>
-      <Text  style={styles.bubble}>
-        Dark Mode :
-      </Text>
-      <Switch
-        trackColor={{ false: "#767577", true: "#81b0ff" }}
-        thumbColor={isDarkModeEnabled ? "#f5dd4b" : "#f4f3f4"}
-        ios_backgroundColor="#3e3e3e"
-        onValueChange={toggleSwitch}
-        value={isDarkModeEnabled}
-      />
-    </View>
-
-  </View>
-)
           {vessels.map((vessel, index) => (
             <Boat
               key={vessel.id}
@@ -331,23 +214,32 @@ const Map = () => {
             />
           ))}
         </MapView>
-      </View>
-        <View style={{
-          width: '100%',
-          height: 30,
-          flexDirection:'row',
-          backgroundColor: '#1e3d59',
-          justifyContent: 'center',
-          alignItems: 'center',
-          position: 'absolute',
-          bottom: 0
-        }}>
-          <Text style={{color:'#ffc13b'}}>{(lastSpeed * METER_TO_KILOMETER_CONSTANT).toFixed(3)} Km/h - </Text>
-          <Text style={{color:'#ffc13b'}}>{(lastSpeed * METER_TO_KNOT_CONSTANT).toFixed(3)} Knots</Text>
+        <View style={styles.speedContainer}>
+          <Text style={styles.bubble}>
+            {lastSpeed * METER_TO_KILOMETER_CONSTANT} Km/h,
+          </Text>
+          <Text style={styles.bubble}>
+            {lastSpeed * METER_TO_KNOT_CONSTANT} Knots
+          </Text>
         </View>
+        <View style={styles.switchContainer}>
+          <Text style={styles.bubble}>
+            Dark Mode :
+          </Text>
+          <Switch
+            trackColor={{false: '#767577', true: '#81b0ff'}}
+            thumbColor={isDarkModeEnabled ? '#f5dd4b' : '#f4f3f4'}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={toggleSwitch}
+            value={isDarkModeEnabled}
+          />
+        </View>
+      </View>
+
+
     </Container>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -355,7 +247,12 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     justifyContent: 'space-between',
     alignItems: 'center',
-    backgroundColor: '#f5f0e1'
+    backgroundColor: '#f5f0e1',
+  },
+  mapContainer: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
   },
   map: {
     ...StyleSheet.absoluteFillObject,
@@ -376,8 +273,8 @@ const styles = StyleSheet.create({
     paddingVertical: 6,
     borderRadius: 20,
     color: 'green',
-    fontSize: 20, fontWeight: 'bold'
+    fontSize: 20, fontWeight: 'bold',
   },
 });
 
-export default Map
+export default Map;
