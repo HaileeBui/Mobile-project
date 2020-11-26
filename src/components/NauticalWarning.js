1c545b18dd8aae93c3c2af6b03664cd5403e72df
Anwar Ulhaq <mailtoanwar@gmail.com>
2020-11-26 23:09:36 +0200

    Added missing files.

---
+++ src/components/NauticalWarning.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { StyleSheet,View, Text, } from 'react-native';
+import {Callout, Marker} from 'react-native-maps';
+
+import nauticalWarningIcon from '../assets/nauticalWarning.png'
+
+const NauticalWarning = ({nauticalWarnings}) => {
+
+  const centerOfImage = {x:0.5,y:0.5}
+
+  return (
+    nauticalWarnings.map( (nauticalWarning, index) => (
+      <Marker
+        key={index}
+        coordinate={{
+          latitude: nauticalWarning.latitude,
+          longitude: nauticalWarning.longitude
+        }}
+        image={nauticalWarningIcon}
+        anchor={centerOfImage}
+      >
+        <Callout style={styles.plainView}>
+          <View>
+            <Text>English: {nauticalWarning.descriptionEN}</Text>
+          </View>
+        </Callout>
+      </Marker>
+    ))
+  )
+}
+
+const styles = StyleSheet.create({
+  plainView: {
+    width: 60,
+  },
+});
+
+export default NauticalWarning;
+
+NauticalWarning.propTypes = {};
+
+NauticalWarning.defaultProps = {};
